82c197db15092d91e2d4e8f898e2e18e42685170
Cédric Uden <cedric@uden.de>
2021-07-26 20:19:30 +0200

    display a basic image

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# importing tkinter for gui
 import tkinter as tk
+from PIL import Image,ImageTk
 
-# creating window
-window = tk.Tk()
-
-# setting attribute
-window.attributes('-fullscreen', True)
-window.title("Geeks For Geeks")
-
-# creating text label to display on window screen
-label = tk.Label(window, text="Hello Tkinter!")
-label.pack()
-
-window.mainloop()
+root = tk.Tk()
+root.title("display image")
+im=Image.open("images/1.jpg")  #This is the correct location and spelling for my image location
+photo=ImageTk.PhotoImage(im)
+cv = tk.Canvas()
+cv.pack(side='top', fill='both', expand='yes')
+cv.create_image(10, 10, image=photo, anchor='nw')
+root.mainloop()
