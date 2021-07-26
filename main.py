96396b610e5505c30115d83936160e422c0e8a7c
Cédric Uden <cedric@uden.de>
2021-07-26 20:46:22 +0200

    resizing the image... not quite but getting there

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import tkinter as tk
-from PIL import Image,ImageTk
+from tkinter import *
+from PIL import ImageTk, Image
 
-root = tk.Tk()
-root.title("display image")
-im=Image.open("images/2.jpg")
-photo=ImageTk.PhotoImage(im)
-cv = tk.Canvas()
-root.attributes('-fullscreen', True)
-cv.pack(side='top', fill='both', expand=True)
-cv.create_image(0, 0, image=photo, anchor='nw')
+image_path = "images/2.jpg"
+
+root = Tk()
+root.title("Dynamically resize background Image")
+# open image file
+bg = ImageTk.PhotoImage(file=image_path)
+# create canvas
+canvas = Canvas(root, width=500, height=500)
+canvas.pack(fill=BOTH, expand=True)
+# place the image inside canvas
+canvas.create_image(0, 0, image=bg, anchor='nw')
+# resize function for resizing the image
+# with proper width and height of root window
+def resize_bg(event):
+    global bgg, resized, bg2
+    # open image to resize it
+    bgg = Image.open(image_path)
+    # resize the image with width and height of root
+    resized = bgg.resize((event.width, event.height),
+                         Image.ANTIALIAS)
+
+    bg2 = ImageTk.PhotoImage(resized)
+    canvas.create_image(0, 0, image=bg2, anchor='nw')
+# bind resized function with root window
+root.bind("<Configure>", resize_bg)
 root.mainloop()
