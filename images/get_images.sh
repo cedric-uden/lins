33788213d1afdd815844b5bc16dd06c57eafad1a
Cédric Uden <cedric@uden.de>
2021-07-26 21:28:13 +0200

    added a script to pull sample images

---
+++ images/get_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# cd to current dir
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && cd $dir
+
+wget -O 1.jpg https://imgsv.imaging.nikon.com/lineup/coolpix/p/p7000/img/sample/img_02_b.jpg
+wget -O 2.jpg https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg
