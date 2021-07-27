290f842e3108db6dff593492fd3a6a57c3694868
Cédric Uden <cedric@uden.de>
2021-07-27 17:36:33 +0200

    now cycles through the existing images automatically on startup... still blocking

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
+import time
 import tkinter as tk
 from PIL import ImageTk
-import threading as th
 
 
-# --- functions ---
-def on_click():
-    load_image()
-    t = th.Timer(5.0, load_image)
-    t.start()
-
-
-def load_image():
+def load_image(current_root):
     global current_image_number
 
     current_image_number = (current_image_number+1) % len(images)
     # change image on canvas
     canvas.itemconfig(image_id, image=images[current_image_number])
+    current_root.update()
 
 
 # --- main ---
 root = tk.Tk()
 
 # canvas for image
-canvas = tk.Canvas(root, width=60, height=60)
+canvas = tk.Canvas(root, width=600, height=600)
 canvas.pack()
-
-# button to change image
-button = tk.Button(root, text="Change", command=on_click)
-button.pack()
 
 # images
 images = [
@@ -39,4 +29,6 @@ current_image_number = 0
 # set first image on canvas
 image_id = canvas.create_image(0, 0, anchor='nw', image=images[current_image_number])
 
-root.mainloop()
+while True:
+    load_image(root)
+    time.sleep(3)
