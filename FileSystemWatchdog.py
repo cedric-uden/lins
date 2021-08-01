2705561d2cb73c2dda1614e2a9fb9f00ff2daaec
Cédric Uden <cedric@uden.de>
2021-08-01 15:20:50 +0200

    fixed the static method

--- FileSystemWatchdog.py
+++ FileSystemWatchdog.py
@@ -2,28 +2,28 @@ from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 
-def on_created(event):
-    print(f"hey, {event.src_path} has been created!")
-
-
 class MyObserver:
 
     patterns = ["*"]
     ignore_patterns = None
     ignore_directories = False
     case_sensitive = True
+    has_file_changes = False
 
     def __init__(self, path):
-
         self.my_event_handler = \
             PatternMatchingEventHandler(self.patterns,
                                         self.ignore_patterns,
                                         self.ignore_directories,
                                         self.case_sensitive)
-        self.my_event_handler.on_created = on_created
         self.path = path
 
+    def on_created(self, event):
+        print(f"hey, {event.src_path} has been created!")
+        self.has_file_changes = True
+
     def start_observer(self):
+        self.my_event_handler.on_created = self.on_created
         go_recursively = True
         my_observer = Observer()
         my_observer.schedule(self.my_event_handler, self.path,
