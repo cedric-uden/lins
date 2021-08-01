dc3beea9bddeb0aa26323e784394da06015c718d
Cédric Uden <cedric@uden.de>
2021-08-01 12:58:33 +0200

    added a third image to pull for testing

--- images/get_images.sh
+++ images/get_images.sh
@@ -5,3 +5,4 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && cd $d
 
 wget -O 1.jpg https://imgsv.imaging.nikon.com/lineup/coolpix/p/p7000/img/sample/img_02_b.jpg
 wget -O 2.jpg https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg
+wget -O 3.jpg https://www.w3schools.com/css/img_mountains.jpg
