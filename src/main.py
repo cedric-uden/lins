344b90a91e9a7f82e74b261857bc5876abccbdf4
Cédric Uden <cedric@uden.de>
2021-08-01 18:58:07 +0200

    refactoring into tinier methods

--- src/main.py
+++ src/main.py
@@ -17,15 +17,20 @@ imh.update_current_run_images()
 my_obs = FileSystemWatchdog.MyObserver(hot_folder_path)
 my_obs.start_observer()
 
+
+def load_new_images(observer, image_handler):
+    new_images = observer.get_and_clean_file_changes_set()
+    image_handler.new_images_not_yet_displayed.update(new_images)
+    image_handler.all_images.update(new_images)
+
+
 imh.load_image()
 ts = time.time()
 print("Init Complete!")
 while True:
     if time.time() - ts > interval_between_images_in_seconds:
         if my_obs.has_file_changes():
-            new_images = my_obs.get_and_clean_file_changes_set()
-            imh.new_images_not_yet_displayed.update(new_images)
-            imh.all_images.update(new_images)
+            load_new_images(my_obs, imh)
 
         if imh.counter_all_displayed_images == len(imh.all_images):
             imh.update_current_run_images()
