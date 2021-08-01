6d49500bee84b7dbd84148ca47b9cd910edc312f
Cédric Uden <cedric@uden.de>
2021-08-01 13:30:53 +0200

    putting settings variables in front

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import time
 import tkinter as tk
 from PIL import ImageTk, Image
 
+canvas_width = 2560
+canvas_height = 1400
+start_x = 1920
+start_y = 1120
+
 
 def load_image():
     global current_image_number
@@ -12,7 +17,7 @@ def load_image():
     pil_image = images[current_image_number]
     img_w, img_h = pil_image.size
     # resize photo to full screen
-    ratio = min(w/img_w, h/img_h)
+    ratio = min(canvas_width / img_w, canvas_height / img_h)
     img_w = int(img_w*ratio)
     img_h = int(img_h*ratio)
     pil_image = pil_image.resize((img_w, img_h), Image.ANTIALIAS)
@@ -25,14 +30,10 @@ def load_image():
 # --- main ---
 root = tk.Tk()
 
-w, h = 2560, 1400
-start_x = 1920
-start_y = 1120
-
 root.overrideredirect(1)
-root.geometry(f"%dx%d+{start_x}+{start_y}" % (w, h))
+root.geometry(f"%dx%d+{start_x}+{start_y}" % (canvas_width, canvas_height))
 root.focus_set()
-canvas = tk.Canvas(root, width=w, height=h)
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
 canvas.pack()
 canvas.configure(background='black')
 
@@ -45,7 +46,9 @@ images = [
 
 
 # create canvas
-image_id = canvas.create_image(w/2, h/2, anchor='center')
+image_id = canvas.create_image(canvas_width / 2,
+                               canvas_height / 2,
+                               anchor='center')
 # and load the first image
 current_image_number = -1
 load_image()
